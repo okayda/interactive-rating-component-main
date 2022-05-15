8cf2953e9982d6821b5dcf0aff7fbdd4ca246356
Jhon <okayda999@gmail.com>
2022-05-15 09:40:47 -0600

    Add improvements & U.I feature

--- rating_functionality.js
+++ rating_functionality.js
@@ -5,25 +5,37 @@ const ratingContainer = document.querySelector(".rating-container");
 const ratedContainer = document.querySelector(".rated-container");
 const userRateEl = document.querySelector(".user-rate");
 
+const errMessage = document.querySelector(".err-msg");
+
 const ratingScale = document.querySelector(".rating-scale");
 const ratingBtn = document.querySelector(".rating-btn");
-let perviousRate;
+let previousRate;
 let userRate;
 
 ratingScale.addEventListener("click", (e) => {
-  if (!e.target.dataset.id) return;
+  const target = e.target;
+  if (!target.dataset.id) return;
 
-  e.target.classList.add("active");
-  if (perviousRate) perviousRate.classList.remove("active");
-  userRate = e.target.dataset.id;
-  perviousRate = e.target;
+  if (previousRate) previousRate.classList.remove("active");
+  if (target === previousRate) {
+    previousRate.classList.remove("active");
+    previousRate = undefined;
+    return;
+  }
+
+  target.classList.add("active");
+  previousRate = target;
+  userRate = target.dataset.id;
 });
 
 ratingBtn.addEventListener("click", (e) => {
-  if (!perviousRate) return;
+  if (!previousRate) {
+    errMessage.classList.add("active");
+    return;
+  }
 
   e.target.disabled = false;
   userRateEl.textContent = userRate;
-  ratingContainer.style.display = "none";
-  ratedContainer.style.display = "block";
+  ratingContainer.classList.remove("active");
+  ratedContainer.classList.add("active");
 });
